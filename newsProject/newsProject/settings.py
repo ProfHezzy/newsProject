8229a2b5a8b9a03242cb84ee7bdfba5d55f839bf
ProfHezzy <107649225+ProfHezzy@users.noreply.github.com>
2025-03-12 14:50:33 +0100

    Update settings.py

--- newsProject/newsProject/settings.py
+++ newsProject/newsProject/settings.py
@@ -75,10 +75,21 @@ WSGI_APPLICATION = 'newsProject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
+'''DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}'''
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'your-database-name',
+        'USER': 'your-username',
+        'PASSWORD': 'your-password',
+        'HOST': 'your-username.mysql.pythonanywhere-services.com',
+        'PORT': '3306',
     }
 }
 
