4b5f7a470b96fafa054ec223db6536fef5133539
ProfHezzy <107649225+ProfHezzy@users.noreply.github.com>
2025-04-17 12:05:11 +0100

    Add files via upload

--- newsProject/newsProject/settings.py
+++ newsProject/newsProject/settings.py
@@ -80,9 +80,10 @@ WSGI_APPLICATION = 'newsProject.wsgi.application'
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-}'''
+}
+'''
 
-DATABASES = {
+'''DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'your-database-name',
@@ -91,8 +92,7 @@ DATABASES = {
         'HOST': 'your-username.mysql.pythonanywhere-services.com',
         'PORT': '3306',
     }
-}
-
+}'''
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
